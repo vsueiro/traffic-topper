42c74df466520598892eae2e2be0e37bd43a49c2
Vinicius Sueiro <hi@vsueiro.com>
2024-06-09 22:46:48 -0400

    Begin bus styling, while adding extra divs for details

--- script.js
+++ script.js
@@ -16,31 +16,36 @@ class Obstacles {
         y: 4,
         h: 2,
         w: 3,
+        details: [],
       },
       bus: {
         allowedPositions: [2],
         y: 2,
         h: 4,
         w: 8,
+        details: ["wheels", "glass"],
       },
-      bike: {
-        allowedPositions: [1, 2],
-        y: 4,
-        h: 2,
-        w: 2,
-      },
+      // bike: {
+      //   allowedPositions: [1, 2],
+      //   y: 4,
+      //   h: 2,
+      //   w: 2,
+      // details: []
+      // },
       signal: {
         allowedPositions: [0, 1],
         y: 0,
         h: 2,
         w: 1,
+        details: [],
       },
-      bridge: {
-        allowedPositions: [0],
-        y: 0,
-        h: 4,
-        w: 12,
-      },
+      // bridge: {
+      //   allowedPositions: [0],
+      //   y: 0,
+      //   h: 4,
+      //   w: 12,
+      //   details: []
+      // },
     };
 
     this.setup();
@@ -245,7 +250,7 @@ class Game {
     this.over = false;
     this.won = false;
 
-    // this.grid = new Grid(this);
+    this.grid = new Grid(this);
     this.taxi = new Taxi(this);
     this.taxiWheels = new TaxiWheels(this);
     this.controls = new Controls(this);
@@ -772,7 +777,7 @@ class Taxi {
     this.x = (1 / this.game.cols) * 1;
     this.speed = {}; // % of screen per second
     this.speed.min = 0.75;
-    this.speed.max = 2.5;
+    this.speed.max = 0.75; //2.5;
     this.speed.x = 0.75;
     this.speed.decreaseX = -1; // % of screen per second
     this.speed.increaseX = 0.1; // % of screen per second
@@ -1206,6 +1211,12 @@ class Obstacle {
     this.element.style.top = `${this.y * 100}%`;
     this.element.style.left = `0%`;
     this.element.style.translate = `calc( var(--cell) * ${this.game.cols} * ${this.x})`;
+
+    for (let detail of this.kind.details) {
+      const div = document.createElement("div");
+      div.classList.add(detail);
+      this.element.append(div);
+    }
 
     this.game.element.append(this.element);
   }
